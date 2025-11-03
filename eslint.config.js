b85c4e7b7c9b968ea66420c8e38e12847c08f1ad
SoniaNvm <173436037+egirlmilkers@users.noreply.github.com>
2025-11-02 16:09:21 -0800

    linting config

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import esx from "eslint-plugin-es-x";
+
+export default defineConfig([
+	// 1. Base ESLint recommended rules (catches common bugs)
+	js.configs.recommended,
+
+	// enforces ES6 (2015) syntax only
+	// will error on ES5 syntax AND any syntax newer than ES6
+	esx.configs["recommended-style-2015"],
+
+	{
+		name: "rermmz/js-es6",
+		files: ["src/**/*.js"],
+		languageOptions: {
+			// Explicitly set the parser to ES6 (2015)
+			ecmaVersion: 2015,
+			sourceType: "module",
+			globals: {
+				// Globals
+				"window": "readonly",
+				"document": "readonly",
+				"effekseer": "readonly"
+			}
+		},
+		rules: {
+			// key ES6 upgrades
+			"no-var": "error", // Fail on 'var'
+			"prefer-const": "error", // Require 'const' or 'let'
+			"object-shorthand": "warn", // { a: a } should be { a }
+			"prefer-arrow-callback": "warn", // Use () => {} for callbacks
+			"prefer-template": "warn", // Use string templates over '+'
+		}
+	},
+
+	{
+		ignores: ["dist/", "local_modules/", "node_modules/"]
+	}
+]);
