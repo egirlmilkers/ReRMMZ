ed29a1dd9629beede12f9e78dc39fbbbce0aac46
SoniaNvm <173436037+egirlmilkers@users.noreply.github.com>
2025-11-04 16:28:06 -0800

    apparently you shouldnt fix interface linting errors

--- src/core/Point.js
+++ src/core/Point.js
@@ -10,14 +10,4 @@ export class Point extends PIXI.Point {
 	constructor(x, y) {
 		super(x, y);
 	}
-
-
-	// Interface methods from PIXI.Point so linter stops complaining
-	x
-	y
-	clone = () => super.clone();
-	copyFrom = (p) => super.copyFrom(p);
-	copyTo = (p) => super.copyTo(p);
-	equals = (p) => super.equals(p);
-	set = (x, y) => super.set(x, y);
 }
